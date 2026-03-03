15b3da83cb8f2fb44fdafce616fad45f0151ab44
KAISER <vipraxus.main@gmail.com>
2026-03-03 06:40:41 +0300

    README added

---
+++ README.md
@@ -0,0 +1,8 @@
+# IN WORK
+This repository is still a work in progress, so many things may change, including this file.
+
+## Algorithms 
+A few algorithms, mostly from the book "Grokking Algorithms" by Aditya Y. Bhargava, and more.
+
+### Requirements
+- Python 3.14
---
+++ quicksort/main.py
@@ -0,0 +1,15 @@
+import random
+
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+arr = list(range(1, 101))
+random.shuffle(arr)
+print(quicksort(arr))
